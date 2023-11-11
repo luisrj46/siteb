0735ce81230ee0a8a916c916880b138586fe640e
Luis <luismeza9508@gmail.com>
2023-11-10 23:55:19 -0500

    actualizando valor de labsmobile

--- config/labsmobile.php
+++ config/labsmobile.php
@@ -3,5 +3,5 @@ return [
     'url' => env('LABSMOBILE_URL', 'https://api.labsmobile.com/json/'),
     'user' => env('LABSMOBILE_USER', ''),
     'token' => env('LABSMOBILE_TOKEN', ''),
-    'topa' => env('LABSMOBILE_TPOA', 'Siteb'),
+    'topa' => env('LABSMOBILE_TPOA', 'Interdoors'),
 ];
