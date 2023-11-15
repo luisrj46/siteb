b515ae6b2a05d4aad79fb54578dc0d01d326cdfb
Luis <luismeza9508@gmail.com>
2023-11-14 22:49:53 -0500

    ajustes en el cron job de mantenimientos

--- app/Console/Commands/DailyMaintenance.php
+++ app/Console/Commands/DailyMaintenance.php
@@ -28,7 +28,7 @@ class DailyMaintenance extends Command
      */
     public function handle()
     {
-        $maintenances = Maintenance::whereRelation('maintenanceType', 'slug', MaintenanceType::PREVENTIVE)->whereDate('scheduled_date', now())->where('created_at', '<', now())->get();
+        $maintenances = Maintenance::whereDate('scheduled_date', now())->whereDate('created_at', '<=', now()->format('Y-m-d 07:00:00'))->get();
         foreach ($maintenances as $maintenance) {
             $maintenance->user?->notify( new MaintenanceNotification($maintenance));
         }
